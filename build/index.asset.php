30d7abf4bb6d7dd90f9f5a781335dba6d457b210
ananya-cedcoss <ananyashukla@makewebbetter.com>
2021-11-05 18:21:13 +0530

    05-nov-2021

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,17 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8bccabc6be0c4de5510ecad1424bc7a4');
+<?php
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the html field for general tab.
+ *
+ * @link       https://makewebbetter.com/
+ * @since      1.0.0
+ *
+ * @package    Membership_For_Woocommerce
+ * @subpackage Membership_For_Woocommerce
+ */
+
+return array(
+	'dependencies' => array( 'react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill' ),
+	'version' => '8bccabc6be0c4de5510ecad1424bc7a4',
+);
