56e493a083652e8cc405d9223850b858ce0987ba
ananya-cedcoss <ananyashukla@makewebbetter.com>
2022-01-20 11:31:01 +0530

    20-jan-2022

--- includes/class-membership-for-woocommerce-onboarding-steps.php
+++ includes/class-membership-for-woocommerce-onboarding-steps.php
@@ -48,7 +48,7 @@ class Membership_For_Woocommerce_Onboarding_Steps {
 	 * @since 1.0.0
 	 * @var string Portal id.
 	 */
-	private static $mwb_mfw_portal_id = '6493626';
+	private static $mwb_mfw_portal_id = '25444144';
 
 	/**
 	 * Form id of hubspot api for membership-for-woocommerce.
@@ -56,7 +56,7 @@ class Membership_For_Woocommerce_Onboarding_Steps {
 	 * @since 1.0.0
 	 * @var string Form id.
 	 */
-	private static $mwb_mfw_onboarding_form_id = 'd94dcb10-c9c1-4155-a9ad-35354f2c3b52';
+	private static $mwb_mfw_onboarding_form_id = '2a2fe23c-0024-43f5-9473-cbfefdb06fe2';
 
 	/**
 	 * Form id of hubspot api for membership-for-woocommerce.
@@ -64,7 +64,7 @@ class Membership_For_Woocommerce_Onboarding_Steps {
 	 * @since 1.0.0
 	 * @var string Form id.
 	 */
-	private static $mwb_mfw_deactivation_form_id = '329ffc7a-0e8c-4e11-8b41-960815c31f8d';
+	private static $mwb_mfw_deactivation_form_id = '67feecaa-9a93-4fda-8f85-f73168da2672';
 
 	/**
 	 * Define some variables for membership-for-woocommerce.
@@ -260,7 +260,6 @@ class Membership_For_Woocommerce_Onboarding_Steps {
 		echo json_encode( 'true' );
 		wp_die();
 	}
-
 
 	/**
 	 * Add your membership-for-woocommerce onboarding form fields.
