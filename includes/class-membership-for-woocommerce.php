845c8391b8b3668aefda9851f97d239d0fd36758
rishabhmwb <rishabhsingh@makewebbetter.com>
2023-01-30 12:14:53 +0530

    version 2.1.8

--- includes/class-membership-for-woocommerce.php
+++ includes/class-membership-for-woocommerce.php
@@ -77,7 +77,7 @@ class Membership_For_Woocommerce {
 			$this->version = MEMBERSHIP_FOR_WOOCOMMERCE_VERSION;
 		} else {
 
-			$this->version = '2.1.7';
+			$this->version = '2.1.8';
 		}
 
 		$this->plugin_name = 'membership-for-woocommerce';
