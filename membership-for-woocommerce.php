b0d57493e25dac329428de8e3609346920d16423
navneet-cedcoss <navneetsingh@makewebbetter.com>
2023-09-22 17:10:16 +0530

    design-bug

--- membership-for-woocommerce.php
+++ membership-for-woocommerce.php
@@ -22,7 +22,7 @@
  * Domain Path:       /languages
  *
  * Requires at least: 5.0
- * Tested up to:      6.2.2
+ * Tested up to:      6.3.1
  * WC requires at least: 5.0
  * WC tested up to:   7.9.0
  *
