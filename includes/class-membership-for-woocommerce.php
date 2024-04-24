46e09f59230cdaba3d0ef53552d25b5d7c89c0b9
navneet-cedcoss <navneetsingh@makewebbetter.com>
2024-04-24 16:47:11 +0530

    version

--- includes/class-membership-for-woocommerce.php
+++ includes/class-membership-for-woocommerce.php
@@ -77,7 +77,7 @@ class Membership_For_Woocommerce {
 			$this->version = MEMBERSHIP_FOR_WOOCOMMERCE_VERSION;
 		} else {
 
-			$this->version = '2.4.0';
+			$this->version = '2.4.1';
 		}
 
 		$this->plugin_name = 'membership-for-woocommerce';
