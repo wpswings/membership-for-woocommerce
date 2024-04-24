48a8a99504c3d4b55e1f99f6976c83a92328554e
navneet-cedcoss <navneetsingh@makewebbetter.com>
2024-04-24 11:45:30 +0530

    version-update

--- membership-for-woocommerce.php
+++ membership-for-woocommerce.php
@@ -22,9 +22,9 @@
  * Domain Path:       /languages
  *
  * Requires at least: 5.0
- * Tested up to:      6.4.3
+ * Tested up to:      6.5.2
  * WC requires at least: 5.0
- * WC tested up to:   8.7.0
+ * WC tested up to:   8.8.2
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
