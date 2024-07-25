e96007ca4457bdd75a1d0324b438bb71d21a302d
navneet-cedcoss <navneetsingh@makewebbetter.com>
2024-07-25 12:52:07 +0530

    wp-wc

--- membership-for-woocommerce.php
+++ membership-for-woocommerce.php
@@ -23,9 +23,9 @@
  * Requires Plugins: woocommerce
  *
  * Requires at least: 5.0
- * Tested up to:      6.5.4
+ * Tested up to:      6.6.1
  * WC requires at least: 5.0
- * WC tested up to:   9.0.1
+ * WC tested up to:   9.1.2
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
