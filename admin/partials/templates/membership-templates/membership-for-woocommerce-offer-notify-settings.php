73334705d29c986a8d219f3bd63efa338bf4276f
navneet-cedcoss <navneetsingh@makewebbetter.com>
2025-03-06 16:36:56 +0530

    phpcs-isses

--- admin/partials/templates/membership-templates/membership-for-woocommerce-offer-notify-settings.php
+++ admin/partials/templates/membership-templates/membership-for-woocommerce-offer-notify-settings.php
@@ -94,8 +94,10 @@ $wps_wpr_offer_message           = get_option( 'wps_wpr_offer_message' );
 					<div class="wps_wpr_enable_offer_setting_wrapper">
 						<textarea class="wps_wpr_offer_message" rows="4" cols="40"><?php echo esc_html( $wps_wpr_offer_message ); ?></textarea>
 						<span class="wps_wpr_enable_offer_notices wps_wpr_label_notice">
-							<!-- translators: %s: whatsapp notify -->
-							<?php echo sprintf( esc_html__( 'This offer message will be sent to the user via WhatsApp, SMS, and Email. You can track the WhatsApp delivery status in the log. To check, %s.', 'membership-for-woocommerce' ), wp_kses_post( $log_url ) ); ?>
+							<?php
+							// translators: %s: whatsapp notify.
+							echo sprintf( esc_html__( 'This offer message will be sent to the user via WhatsApp, SMS, and Email. You can track the WhatsApp delivery status in the log. To check, %s.', 'membership-for-woocommerce' ), wp_kses_post( $log_url ) );
+							?>
 						</span>
 					</div>
 				</article>
