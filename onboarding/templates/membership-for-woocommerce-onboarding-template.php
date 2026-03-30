b2d12ff380cea5379ee8359b9e0a8963c89943de
ananya-cedcoss <ananyashukla@makewebbetter.com>
2026-03-30 11:56:14 +0530

    onboarding form updated

--- onboarding/templates/membership-for-woocommerce-onboarding-template.php
+++ onboarding/templates/membership-for-woocommerce-onboarding-template.php
@@ -12,45 +12,48 @@
  */
 
 global $mfw_wps_mfw_obj;
-$mfw_onboarding_form_fields =
+  $mfw_onboarding_form_fields =
+  apply_filters( 'wps_mfw_on_boarding_form_fields',
+  array() );
+  ?>
 
-/**
- * Filter for form field.
- *
- * @since 1.0.0
- */
-apply_filters( 'wps_mfw_on_boarding_form_fields', array() );
-?>
+  <?php if ( ! empty( $mfw_onboarding_form_fields ) ) : ?>
+        <div class="wps-mfw-onboarding-overlay">
+                <div class="wps-mfw-onboarding-card">
+                        <div class="wps-mfw-onboarding-head">
+                                <h3><?php esc_html_e( 'Welcome to WP Swings', 'membership-
+  for-woocommerce' ); ?></h3>
+                                <button class="wps-mfw-onboarding-close" aria-label="<?php
+  esc_attr_e( 'Close onboarding', 'membership-for-
+  woocommerce' ); ?>">
+                                        <span class="material-icons">close</span>
+                                </button>
+                        </div>
 
-<?php if ( ! empty( $mfw_onboarding_form_fields ) ) : ?>
-	<div class="mdc-dialog mdc-dialog--scrollable <?php echo
-	wp_kses_post( apply_filters('wps_stand_dialog_classes', 'membership-for-woocommerce' ) ); ?>">
-		<div class="wps-mfw-on-boarding-wrapper-background mdc-dialog__container">
-			<div class="wps-mfw-on-boarding-wrapper mdc-dialog__surface" role="alertdialog" aria-modal="true" aria-labelledby="my-dialog-title" aria-describedby="my-dialog-content">
-				<div class="mdc-dialog__content">
-					<div class="wps-mfw-on-boarding-close-btn">
-						<a href="#"><span class="mfw-close-form material-icons wps-mfw-close-icon mdc-dialog__button" data-mdc-dialog-action="close">clear</span></a>
-					</div>
-					<h3 class="wps-mfw-on-boarding-heading mdc-dialog__title"><?php esc_html_e( 'Welcome to WP Swings', 'membership-for-woocommerce' ); ?> </h3>
-					<p class="wps-mfw-on-boarding-desc"><?php esc_html_e( 'We love making new friends! Subscribe below and we promise to keep you up-to-date with our latest new plugins, updates, awesome deals and a few special offers.', 'membership-for-woocommerce' ); ?></p>
+                        <p class="wps-mfw-onboarding-intro"><?php esc_html_e( 'We
+  love making new friends! Subscribe below and we promise to
+  keep you up-to-date with our latest new plugins, updates,
+  awesome deals and a few special offers.', 'membership-for-
+  woocommerce' ); ?></p>
 
-					<form action="#" method="post" class="wps-mfw-on-boarding-form">
-						<?php
-						$mfw_onboarding_html = $mfw_wps_mfw_obj->wps_mfw_plug_generate_html( $mfw_onboarding_form_fields );
-						echo esc_html( $mfw_onboarding_html );
-						?>
-						<div class="wps-mfw-on-boarding-form-btn__wrapper mdc-dialog__actions">
-							<div class="wps-mfw-on-boarding-form-submit wps-mfw-on-boarding-form-verify ">
-								<input type="submit" class="wps-mfw-on-boarding-submit wps-on-boarding-verify mdc-button mdc-button--raised" value="Send Us">
-							</div>
-							<div class="wps-mfw-on-boarding-form-no_thanks">
-								<a href="#" class="wps-mfw-on-boarding-no_thanks mdc-button" data-mdc-dialog-action="discard"><?php esc_html_e( 'Skip For Now', 'membership-for-woocommerce' ); ?></a>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-		<div class="mdc-dialog__scrim"></div>
-	</div>
-<?php endif; ?>
+                        <form class="wps-mfw-onboarding-form" action="#"
+  method="post">
+                                <?php
+                                $mfw_onboarding_html =
+  $mfw_wps_mfw_obj->wps_mfw_plug_generate_html( $mfw_onboarding_form_fields );
+                                echo wp_kses_post( $mfw_onboarding_html );
+                                ?>
+                                <div class="wps-mfw-onboarding-actions">
+                                        <input type="submit" class="wps-mfw-onboarding-submit"
+  value="<?php esc_attr_e( 'Send Us', 'membership-for-
+  woocommerce' ); ?>">
+                                        <a class="wps-mfw-onboarding-skip" href="#" data-mdc-
+  dialog-action="discard"><?php esc_html_e( 'Skip For Now',
+  'membership-for-woocommerce' ); ?></a>
+                                </div>
+                        </form>
+                </div>
+        </div>
+  <?php endif; ?>
+
+
